74d6ba6f810b54abfab64dab03dcf920af994ba3
Kevin Bost <kevin@intellitect.com>
2019-09-18 22:10:06 -0700

    Updating to XF 4.2 Adding small example app.

---
+++ Example/Example/ColorConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Proxy;
+
+namespace Example
+{
+    public class ColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values?.Length == 3 && values.Select(x => x?.ToString()).All(x => x != null))
+            {
+                return Color.FromHex(string.Join("", values));
+            }
+            return Color.Transparent;
+        }
+    }
+}
